6b32c87d5ab1a14c06e70c5462672868539df340
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2022-04-06 07:08:39 +0530

    Update urlController.js

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -12,8 +12,9 @@ const isValidRequestBody = (requestBody) =>{
 }
 const createUrl = async function (req, res){
     try{
-     let longUrl = req.body.longUrl
     const baseUrl = 'http:localhost:3000'
+     let longUrl = req.body.longUrl
+    
      if(!isValidRequestBody(req.body)){
          return res.status(400).send({status:false,message:'Invalid request body'})
      }
@@ -23,9 +24,6 @@ const createUrl = async function (req, res){
      if(!validUrl.isUri(longUrl)){
         return res.status(400).send({status:false, message: 'Invalid longUrl'})
     }
-    if(!isValid(baseUrl)){
-        return res.status(400).send({status:false, message: 'Please provide baseUrl'})
-     }
     if(!validUrl.isUri(baseUrl)){
         return res.status(400).send({status:false, message: 'Invalid baseUrl'})
     }
@@ -37,9 +35,13 @@ const createUrl = async function (req, res){
         "longUrl":longUrl,
         "shortUrl":shortUrl
 }
-   let createUrl = await urlModel.create(obj)
-   return res.status(201).send({status:true, data:createUrl})
-   
+let longUrlexist= await urlModel.findOne({longUrl}).select()
+if(longUrlexist){
+    return res.status(200).send({status:true,data:longUrlexist})
+}else{
+ let createUrl = await urlModel.create(obj)
+return res.status(201).send({status:true, data:createUrl})
+} 
     
     }catch(err){
         return res.status(500).send({status:false,err:err.message})
@@ -48,11 +50,11 @@ const createUrl = async function (req, res){
 const getUrl = async function(req, res) {
     try{
         let urlCode = req.params
-    let originalUrl = await urlModel.findOne(urlCode).select({longUrl:1})
+    let originalUrl = await urlModel.findOne(urlCode)
     if(!originalUrl){
         return res.status(400).send({status:false,message:"url not found"})
     }else{
-        return res.status(301).redirect(originalUrl.longUrl)
+        return res.status(302).redirect(originalUrl.longUrl)
     }
 }catch(err){
     return res.status(500).send({status:false,err:err.message})
